5d3e58129dbe08eb1a4c2041ad855abd8e8d4741
Ricardo Brito <@britodfbr>
2017-06-22 21:10:15 -0300

    aplicação com fracionamento em mvc

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,11 +1,19 @@
 from app import app
 
-
+@app.route('/home')
+@app.route('/index')
 @app.route("/")
 def index():
     return "Página Principal"
 
-
 @app.route("/hello")
 def hello():
     return "Hello World!"
+
+@app.route('/teste', defaults={'name': None})
+@app.route('/teste/<name>')
+def teste(name):
+    if name:
+        return 'Olá, %s!' % name
+    else:
+        return 'Olá usuário!'
