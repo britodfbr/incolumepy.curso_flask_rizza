768db18fcdfcdf68bb833bfc463b94fc02a2b08a
Ricardo Brito <@britodfbr>
2018-03-18 23:26:10 -0300

    estrutura inicial em construção

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -4,8 +4,9 @@ try:
     from app import app
 except ImportError:
     sys.path.append(os.path.abspath('../'))
-    print(os.path.abspath('../'))
-    from app import app
 
-if __name__ == '__main__':
-    app.run()
+
+
+@app.route("/")
+def index():
+    return "Hello World!"
