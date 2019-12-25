4492e3b3cbf37082d91ee2eebed045bfb223df7b
@britodfbr <britodfbr@gmail.com>
2019-12-25 09:27:48 -0300

    Update version: 0.0.1 → 0.0.2-dev0

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,6 @@ template_dir = os.path.join(os.path.dirname(__file__), 'views', 'templates')
 static_dir = os.path.join(os.path.dirname(__file__), 'views', 'static')
 
 app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
-__version__ = '0.0.1'
+__version__ = '0.0.2-dev0'
 
 from app.controllers import default
