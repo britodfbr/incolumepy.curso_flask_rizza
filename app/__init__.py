1fd77f4b92baddf955261a01197b639120afd509
@britodfbr <britodfbr@gmail.com>
2019-12-25 09:17:05 -0300

    Controlar versão com bumpversion

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 import os
 from flask import Flask
 
-template_dir = os.path.join(os.path.dirname(__file__), 'views', 'templates')
-static_dir = os.path.join(os.path.dirname(__file__), 'views', 'static')
-app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
+# template_dir = os.path.join(os.path.dirname(__file__), 'views', 'templates')
+# static_dir = os.path.join(os.path.dirname(__file__), 'views', 'static')
+
+# app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
+__version__ = '0.0.0-dev0'
+app = Flask(__name__)
 
 from app.controllers import default
