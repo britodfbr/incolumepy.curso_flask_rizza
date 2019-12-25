ac1029d1e76fcbe38006bfdcc37a0878886c4dc4
@britodfbr <britodfbr@gmail.com>
2019-12-25 09:10:39 -0300

    import conforme py3

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,6 @@ import os
 import sys
 from flask import Flask
 
-sys.path.append(os.path.abspath('../'))
-#print(sys.path)
 app = Flask(__name__)
 
 from app.controllers import default
--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,10 +1,6 @@
 import os
 import sys
-try:
-    from app import app
-except ImportError:
-    sys.path.append(os.path.abspath('../'))
-
+from app import app
 
 
 @app.route("/")
