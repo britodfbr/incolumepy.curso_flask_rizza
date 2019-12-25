554cf85e0a58ea626b04c9a32113cb51bd021fc4
@britodfbr <britodfbr@gmail.com>
2019-12-25 12:24:10 -0300

    Update version: 0.0.5 → 0.0.6-dev0

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 
-__version__ = '0.0.5'
+__version__ = '0.0.6-dev0'
 template_dir = os.path.join(os.path.dirname(__file__), 'views', 'templates')
 static_dir = os.path.join(os.path.dirname(__file__), 'views', 'static')
 
