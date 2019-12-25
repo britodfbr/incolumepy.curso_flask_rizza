49877634aa34e220d7ea54d19f123a0345dec953
@britodfbr <britodfbr@gmail.com>
2019-12-25 10:15:51 -0300

    Update version: 0.0.2-dev0 → 0.0.2-dev1

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,6 @@ static_dir = os.path.join(os.path.dirname(__file__), 'views', 'static')
 app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////storage.db'
 db = SQLAlchemy(app)
-__version__ = '0.0.2-dev0'
+__version__ = '0.0.2-dev1'
 
 from app.controllers import default
