e0eab8f16d4c2e4815304574f50d30d26f57be00
@britodfbr <britodfbr@gmail.com>
2019-12-25 09:27:11 -0300

    views em diretório personalizados

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,10 @@
 import os
 from flask import Flask
 
-# template_dir = os.path.join(os.path.dirname(__file__), 'views', 'templates')
-# static_dir = os.path.join(os.path.dirname(__file__), 'views', 'static')
+template_dir = os.path.join(os.path.dirname(__file__), 'views', 'templates')
+static_dir = os.path.join(os.path.dirname(__file__), 'views', 'static')
 
-# app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
+app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
 __version__ = '0.0.1'
-app = Flask(__name__)
 
 from app.controllers import default
