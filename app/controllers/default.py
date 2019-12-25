465116f8480366301ba977c2453bd2c8de2a63c6
@britodfbr <britodfbr@gmail.com>
2019-12-25 11:07:20 -0300

    Aula 6 - Rotas - Curso de Flask, qua dez 25 11:07:20 -03 2019

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -3,6 +3,12 @@ import sys
 from app import app
 
 
+@app.route('/index')
 @app.route("/")
 def index():
     return "Hello World!"
+
+
+@app.route("/teste")
+def teste():
+    return 'Olá!'
--- docs/extras.md
+++ docs/extras.md
@@ -4,3 +4,7 @@ python run.py db init
 python run.py db migrate
 python run.py db upgrade
 ```
+
+### Rodar o servidor em foreground
+```python run.py runserver``
+`
