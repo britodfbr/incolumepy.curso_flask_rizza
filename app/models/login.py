5c808767555df4ab85ff41c7fa5f95320f52ad0c
@britodfbr <britodfbr@gmail.com>
2019-12-26 19:05:39 -0300

    Correção de bug

--- app/models/login.py
+++ app/models/login.py
@@ -7,5 +7,5 @@ from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
     username = StringField('username', validators=[DataRequired()])
-    password = PasswordField('password', validators=[DataRequired])
+    password = PasswordField('password', validators=[DataRequired()])
     rememberme = BooleanField('rememberme')
