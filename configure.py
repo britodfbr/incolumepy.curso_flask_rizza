b405f8e00d48e508b02bc62315074d18c979f255
@britodfbr <britodfbr@gmail.com>
2019-12-27 00:55:01 -0300

    Configuração path sgbd

--- configure.py
+++ configure.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 DEBUG = True
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_DATABASE_URI = 'sqlite:///storage.db'
+SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(basedir, 'storage.db')}"
 SECRET_KEY = r'4d0f58ca6423f3b21577d7af4db4e526'
